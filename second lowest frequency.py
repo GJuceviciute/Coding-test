3ce41f5f22a5ad076a7e25d4be68e0b175352b27
GJuceviciute <57292801+GJuceviciute@users.noreply.github.com>
2020-07-02 15:23:26 +0100

    Create second lowest frequency.py

---
+++ second lowest frequency.py
@@ -0,0 +1,24 @@
+def secondLowest(nums):
+    if not nums:
+        return
+    set_nums = set(nums)
+    if len(set_nums) == 1:
+        return nums[0]
+    nums.sort()
+    if len(set_nums) == len(nums):
+        return nums[1]
+    d = {}
+    for i in nums:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    frequencies = sorted(set(d.values()))
+    if len(frequencies) == 1:
+        second_lowest_frequency = frequencies[0]
+    else:
+        second_lowest_frequency = frequencies[1]
+    low_frequency_elements = sorted([i for i in d if d[i] == second_lowest_frequency])
+    if len(low_frequency_elements) == 1:
+        return low_frequency_elements[0]
+    return low_frequency_elements[1]
