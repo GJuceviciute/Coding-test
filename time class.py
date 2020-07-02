806f03ffff049e5424020b16a473e940414da3ff
GJuceviciute <57292801+GJuceviciute@users.noreply.github.com>
2020-07-02 14:52:22 +0100

    Add files via upload

---
+++ time class.py
@@ -0,0 +1,33 @@
+class Time:
+    def __init__(self, hours=0, minutes=0, seconds=0):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def scale(self, seconds):
+        self.seconds += seconds
+        self.minutes += self.seconds//60
+        self.seconds %= 60
+        self.hours += self.minutes//60
+        self.minutes %= 60
+        self.hours %= 24
+
+    def timeString(self):
+        Time.scale(self, 0)
+        h = str(self.hours)
+        m = str(self.minutes)
+        s = str(self.seconds)
+        if self.hours < 10:  # could use built-in string formatting to add leading 0s
+            h = '0' + h
+        if self.minutes < 10:
+            m = '0' + m
+        if self.seconds < 10:
+            s = '0' + s
+        # return h + ':' + m + ':' + s
+        return ':'.join([h, m, s])
+
+
+t = Time(23, 59, 60)
+print(t.timeString())
+print(t.scale(60*60+1))
+print(t.timeString())
