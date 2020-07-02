53625ab42dd0753a4067083c7280c402c3cd7744
GJuceviciute <57292801+GJuceviciute@users.noreply.github.com>
2020-07-02 15:24:01 +0100

    Create savings account balance.py

---
+++ savings account balance.py
@@ -0,0 +1,9 @@
+def balance(openingSum, interestRate, taxFreeLimit, taxRate, numMonths):
+    cur_balance = openingSum
+    for _ in range(numMonths):
+        interest = cur_balance * (interestRate / 100)
+        tax = 0
+        if cur_balance > taxFreeLimit:
+            tax = (cur_balance - taxFreeLimit) * (taxRate / 100)
+        cur_balance = cur_balance + interest - tax
+    return cur_balance
